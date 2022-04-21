6cfcd7bdb37213f73f09174be2d875958c1ad557
Anji <nhu.nhu.face@gmail.com>
2022-04-21 18:10:54 +0900

    share screen while show webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { io } from "socket.io-client";
 import Room from "./Room";
+import ShareScreen from "./ShareScreen";
 
 interface RoomProps {
   children?: React.ReactNode;
@@ -14,9 +15,24 @@ export const socket = io(import.meta.env.VITE_SERVER, {
   reconnection: false,
 });
 
+interface IGlobalContext {
+  screenId: string;
+  setScreenId: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const GlobalContext = React.createContext<IGlobalContext>({
+  screenId: "",
+  setScreenId: () => {},
+});
+
 const App: React.FC<RoomProps> = ({ children }) => {
   const [media, setMedia] = useState<MediaStream | null>(null);
+  const [type, setType] = useState<"camera" | "screen">("screen");
+  const [shareScreen, setShareScreen] = useState(false);
+  const [screenId, setScreenId] = useState("");
+
   const getStream = (type: "screen" | "camera") => {
+    setType(type);
     switch (type) {
       case "camera":
         navigator.mediaDevices.getUserMedia(mediaSetting).then((media) => {
@@ -34,24 +50,46 @@ const App: React.FC<RoomProps> = ({ children }) => {
     }
   };
   return (
-    <div>
-      <button
-        onClick={() => {
-          getStream("camera");
-        }}
-      >
-        Use Camera
-      </button>
-      <button
-        onClick={() => {
-          getStream("screen");
-        }}
-      >
-        Use Webcam
-      </button>
-
-      {media && <Room media={media} />}
-    </div>
+    <GlobalContext.Provider value={{ screenId, setScreenId }}>
+      {type === "camera" && (
+        <>
+          <button
+            onClick={() => {
+              setShareScreen(true);
+            }}
+          >
+            Screen Sharing
+          </button>
+        </>
+      )}
+      {media ? (
+        <>
+          <Room media={media} />
+          {shareScreen && (
+            <>
+              <ShareScreen />
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <button
+            onClick={() => {
+              getStream("camera");
+            }}
+          >
+            Use Camera
+          </button>
+          <button
+            onClick={() => {
+              getStream("screen");
+            }}
+          >
+            Share Screen
+          </button>
+        </>
+      )}
+    </GlobalContext.Provider>
   );
 };
 export default App;
